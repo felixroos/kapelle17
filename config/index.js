dcecf71f1347aa6b81d6da710737298c87a10a7a
Felix Roos <roos@entrecode.de>
2018-06-04 13:23:53 +0200

    fixed assets for build env

--- config/index.js
+++ config/index.js
@@ -8,8 +8,8 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'kapelle17/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: './static',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'kapelle17/static',
     assetsPublicPath: '/',
 
     /**
