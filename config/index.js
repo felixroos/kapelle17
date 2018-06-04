1cf4fe731a1bb249709be9dff51812e9afdb8469
Felix Roos <roos@entrecode.de>
2018-06-04 13:34:16 +0200

    try another path

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'kapelle17/static',
+    assetsSubDirectory: './static',
     assetsPublicPath: '/',
 
     /**
