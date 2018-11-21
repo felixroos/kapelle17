fc7cc71c3c3535b5743c2acac39573d023cccd35
Felix Roos <roos@entrecode.de>
2018-11-21 10:47:24 +0100

    embeded bandcamp

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="hello">
     <h1>Media</h1>
+    <!-- <h2>Youtube</h2> -->
     <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLwBdrxA3ObS0TlK7ZUhwbh0YSjhh845wJ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <!-- https://www.youtube.com/playlist?list=PLwBdrxA3ObS0TlK7ZUhwbh0YSjhh845wJ -->
+    <!-- <h2>Bandcamp</h2> -->
+    <iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=1287943275/size=large/bgcol=ffffff/linkcol=e99708/tracklist=false/transparent=true/" seamless><a href="http://kapelle17.bandcamp.com/album/music-for-young-business-people">Music For Young Business People by kapelle17</a></iframe>
     <!-- https://soundcloud.com/johannes-mann/trepak-livekorridor -->
+    <!-- <h2>Soundcloud</h2> -->
     <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/546877041&color=%23414030&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     <!-- https://www.youtube.com/watch?v=dGnipWz1sXY -->
   </div>
